4352f0178369131a5f84b22082ec805e4dbfa120
Marcus <marcus.potts@hotmail.com>
2016-04-09 11:52:14 +1000

    added

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$( document ).ready()
+$('h1').addClass('animated fadeIn');
+$('h2').addClass('animated fadeIn');
+$('li').addClass('animated fadeInUp');
+
+$('#image').addClass('animated fadeIn');
+$('.h').addClass('animated fadeIn');
+$('p').addClass('animated fadeIn');
+$('#bg').addClass('animated fadeIn');
+$('#icons').addClass('animated fadeInUp');
+$('#logo').addClass('animated fadeIn');
+
